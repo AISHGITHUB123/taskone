bbf22f26e433062ca4c1a2ab1425075242da8496
AISHGITHUB123 <105639937+AISHGITHUB123@users.noreply.github.com>
2022-12-05 01:24:06 +0530

    Create main.tf

---
+++ modules/compute/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_availability_set" "web_availabilty_set" {
+  name                = "web_availabilty_set"
+  location            = var.location
+  resource_group_name = var.resource_group
+}
+
+resource "azurerm_network_interface" "web-net-interface" {
+    name = "web-network"
+    resource_group_name = var.resource_group
+    location = var.location
+
+    ip_configuration{
+        name = "web-webserver"
+        subnet_id = var.web_subnet_id
+        private_ip_address_allocation = "Dynamic"
+    }
+}
